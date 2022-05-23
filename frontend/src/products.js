b0103ff909a512306e3b7df7dcae6d6fbb4c806d
fadi <fadi.alset.94@gmail.com>
2022-05-23 12:11:03 +0200

    created product component and homescreen with random data

---
+++ frontend/src/products.js
@@ -0,0 +1,70 @@
+const products = [
+  {
+    _id: '1',
+    name: 'Xiaomi Mi Smart Band 6',
+    image: '/images/xioami.jpg',
+    description:
+      'Xiaomi Mi Smart Band 6, Global versie, activiteitstracker, zuurstofdetectie in bloed, hartslagmeter, slaapmonitor, AMOLED-kleurendisplay, 3,9 cm (1,56 inch), 5 ATM, zwart',
+    brand: 'Xiaomi',
+    category: 'Electronics',
+    price: 37.5,
+    countInStock: 10,
+    rating: 4.5,
+    numReviews: 12,
+  },
+  {
+    _id: '2',
+    name: 'GARMIN Approach S12',
+    image: '/images/GARMIN.jpg',
+    description:
+      'GARMIN Approach S12, Golf Horloge, Health Smartwatch, GPS, Met Afstandsmeting en Green Weergave, Baankaarten, 1,3-Inch Display, 30 uur Batterijduur',
+    brand: 'GARMIN',
+    category: 'Electronics',
+    price: 109.99,
+    countInStock: 7,
+    rating: 4.0,
+    numReviews: 8,
+  },
+  {
+    _id: '3',
+    name: 'Galaxy Watch4 Classic',
+    image: '/images/galaxy.png',
+    description:
+      'Stylish design including rotating bezel Endless apps endless possibilitiesCheck your health in an instantA powerhouse on your wrist',
+    brand: 'Samsung',
+    category: 'Electronics',
+    price: 235.99,
+    countInStock: 5,
+    rating: 3,
+    numReviews: 12,
+  },
+  {
+    _id: '4',
+    name: 'Nordväl SW103B',
+    image: '/images/nordval.jpg',
+    description:
+      'This powerful and versatile smartwatch provides you with detailed health data, sports and fitness modes, social media notifications and more, packed in a waterproof design (IP67) with long lasting battery life.',
+    brand: 'Nordval',
+    category: 'Electronics',
+    price: 49.99,
+    countInStock: 11,
+    rating: 5,
+    numReviews: 12,
+  },
+  {
+    _id: '5',
+    name: 'Apple Watch Series 7 - 45mm - Midnight',
+    image: '/images/apple.jpg',
+    description:
+      'Introducing the Apple Watch Series 7. This Apple Watch measures oxygen in your blood with a groundbreaking new sensor and app. Take an EKG when and where it suits you and view your workout data at a glance on the improved always-on Retina display. With Apple Watch Series 7 on your wrist, a healthier, more active, more connected life is at your fingertips. The smartwatch has an aluminum case that is lightweight and made of high-quality 100% recycled aluminum.',
+    brand: 'Apple',
+    category: 'Electronics',
+    price: 415.00,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  
+]
+
+export default products
